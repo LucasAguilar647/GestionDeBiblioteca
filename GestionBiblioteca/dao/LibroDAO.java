0418bfdf1a265ee6aa57d843b8b3e5b3e89bc787
Agustín <agus.aguirre.pro@gmail.com>
2025-07-21 15:16:05 -0300

    Added LibroDao main methods

--- GestionBiblioteca/dao/LibroDAO.java
+++ GestionBiblioteca/dao/LibroDAO.java
@@ -1,5 +1,24 @@
 package GestionBiblioteca.dao;
 
+import GestionBiblioteca.entity.Libro;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.Map;
+import java.util.Optional;
+
 public class LibroDAO {
-    
+    private final Map<Long, Libro> data;
+
+    public LibroDAO(Map<Long, Libro> data) {
+        this.data = data;
+    }
+
+
+    public Optional<Libro> findById(Long id) {
+        throw new ExecutionControl.NotImplementedException("");
+    }
+
+    public void Save(Libro newLibro) {
+        throw new ExecutionControl.NotImplementedException("");
+    }
 }
